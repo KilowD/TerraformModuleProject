1657b99a6afaeca0c6ed1bab14e630cc1875a78d
tendai <tendai.pasipamire@standardbank.co.za>
2022-08-22 20:15:04 +0200

    first commit

---
+++ modules/nat-gateway/variables.tf
@@ -0,0 +1,8 @@
+variable "public-subnet-az1-id" {}
+variable "public-subnet-az2-id" {}
+variable "internet_gateway" {}
+variable "vpc-id" {}
+variable "private_app_subnet_az1-id"{}
+variable "private_data_subnet_az1-id" {}
+variable "private_app_subnet_az2-id"{}
+variable "private_data_subnet_az2-id" {}
---
+++ ynwa-website/variables.tf
@@ -0,0 +1,12 @@
+
+# VPC Variables
+variable "region" {}
+variable "project_name" {}
+variable "vpc-cidr" {}
+variable "public-subnet-az1-cidr" {}
+variable "public-subnet-az2-cidr" {}
+variable "private_app_subnet_az1-cidr" {}
+variable "private_app_subnet_az2-cidr" {}
+variable "private_data_subnet_az1-cidr" {}
+variable "private_data_subnet_az2-cidr" {}
+
